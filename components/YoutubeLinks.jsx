438881b44db5c2ce90a951ef7c23de3be5c475a1
park <park>
2023-12-25 08:46:22 +0900

    Fix key warning in YoutubeLinks component

--- components/YoutubeLinks.jsx
+++ components/YoutubeLinks.jsx
@@ -2,8 +2,8 @@ import YoutubeLink from "./YoutubeLink";
 export default function YoutubeLinks(videos) {
   return (
     <>
-      {Object.entries(videos.videos).map((video) => (
-        <YoutubeLink video={video}/>
+      {Object.entries(videos.videos).map((video, i) => (
+        <YoutubeLink video={video} key={i}/>
       ))}
     </>
   );
