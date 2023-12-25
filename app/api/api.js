3243bae73f237d74b84fa08d96cef7a33711cfd9
park <park>
2023-12-25 09:07:24 +0900

    Update API URL

--- app/api/api.js
+++ app/api/api.js
@@ -1,3 +1,3 @@
 export const apiUrl = process.env.NODE_ENV === 'production'
-    ? 'https://next-mongo-crud-jade.vercel.app/api'
+    ? 'https://nextmadmovie-i6eodt189-danciingqueen.vercel.app/api'
     : 'http://localhost:3000/api';
