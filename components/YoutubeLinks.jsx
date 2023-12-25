086437089c56145d0d0ff0589adfe915032dbf58
park <park>
2023-12-25 09:08:57 +0900

    Refactor YoutubeLinks component

--- components/YoutubeLinks.jsx
+++ components/YoutubeLinks.jsx
@@ -1,11 +1,11 @@
 import YoutubeLink from "./YoutubeLink"
 export default function YoutubeLinks(videos) {
-  // Object.entries(videos.videos).map((video, i) => {
-  //   console.log(video, i)
+  // Object.entries(videos.videos).map((video) => {
+  //   console.log(video[1]._id)
   // })
   return (
     <>
-      {Object.entries(videos.videos).map((video, i) => (
+      {Object.entries(videos.videos).map((video) => (
         <YoutubeLink video={video} key={video[1]._id}/>
       ))}
     </>
